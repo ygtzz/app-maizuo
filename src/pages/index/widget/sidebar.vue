4a1ebadb520067fd9d58275e396180ec0ee20641
mengwei <mengwei@papayamobile.cn>
2017-01-17 17:56:58 +0800

    simplify sidebar tag hierarchy

--- src/pages/index/widget/sidebar.vue
+++ src/pages/index/widget/sidebar.vue
@@ -1,19 +1,17 @@
 <template>
-    <aside @click="fToggleSideBar()" v-show="bShow" class="sidebar">
-        <div class="slide-overlay">
-            <transition name="leftNav">
-                <nav v-show="bShow" class="nav">
-                    <ul class="list">
+        <aside @click="fToggleSideBar()" v-show="bShow" class="sidebar">
+            <nav class="nav">
+                <transition name="leftNav">
+                    <ul v-show="bShow" class="list">
                         <li class="item"><span>首页</span><i class="fa fa-angle-right"></i></li>
                         <li class="item"><span>影片</span><i class="fa fa-angle-right"></i></li>
                         <li class="item"><span>影院</span><i class="fa fa-angle-right"></i></li>
                         <li class="item"><span>我的</span><i class="fa fa-angle-right"></i></li>
                         <li class="item"><span>卖座网查询</span><i class="fa fa-angle-right"></i></li>
                     </ul>
-                </nav>
-            </transition>
-        </div>
-    </aside>
+                </transition>
+            </nav>
+        </aside>
 </template>
 
 <script>
@@ -40,11 +38,10 @@ export default{
          transition:transform ease .4s;
      }
      .leftNav-enter,.leftNav-leave-to{
-         transform:translateX(-100%)
+         transform:translateX(-100%);
      }
     .sidebar{position:fixed;top:0;left:0;right:0;bottom:0;background:rgba(0,0,0,.5);}
-    .slide-overlay{position:absolute;top:50px;right:0;left:0;bottom:0;}
-    .nav{position:absolute;top:0;right:110px;left:0;bottom:0;background:#282828;color:#9a9a9a;}
-    .list{list-style:none;padding:0;margin:0}
+    .nav,.list{position:absolute;top:0;right:0;left:0;bottom:0;}
+    .list{top:50px;right:110px;padding:0;margin:0;list-style:none;background:#282828;color:#9a9a9a;}
     .item{padding:15px;display:flex;justify-content:space-between;border:1px #333 dotted;}
 </style>
