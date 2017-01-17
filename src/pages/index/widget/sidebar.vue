74f5d1a8cb6fce8dbbad8a7ae4334890ba3960f3
mengwei <mengwei@papayamobile.cn>
2017-01-17 18:02:58 +0800

    sidebar style add scoped

--- src/pages/index/widget/sidebar.vue
+++ src/pages/index/widget/sidebar.vue
@@ -33,7 +33,7 @@ export default{
 }
 </script>
 
-<style lang="sass" >
+<style lang="sass" scoped>
      .leftNav-enter-active,.leftNav-leave-active{
          transition:transform ease .4s;
      }
